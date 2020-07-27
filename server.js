df600d4f030696401cd277b21c9511e38c5a341e
Anandmayi <tosendanand@gmail.com>
2020-07-28 00:17:29 +0530

    downloaded socket io cdn js file

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+var http = require('http').createServer(app);
+
+http.listen(port, function () {
+    console.log('listening on localhost:3000');
+});
 let connectedPeers = new Map()
 //keping a reference of all the connections and there passstrings
 var peers = io.of('/my-namespace');
@@ -34,8 +39,3 @@ peers.on('connection', socket => {
         };
     })
 })
-var http = require('http').createServer(app);
-
-http.listen(port, function () {
-    console.log('listening on localhost:3000');
-});
