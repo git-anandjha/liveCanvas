313950882f2d3fc9108e24ef5fb57f66ab140f51
Anandmayi <tosendanand@gmail.com>
2020-07-28 00:24:00 +0530

    modifying server.js file

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-var http = require('http').createServer(app);
+var http = require('http').Server(app);
 
 http.listen(port, function () {
     console.log('listening on localhost:3000');
